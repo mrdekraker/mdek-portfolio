381db80f6e7be34612cd5d82f80f4fbb18f33539
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-11-05 13:11:39 -0500

    added scrolling function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ const btn = document.querySelector('button');
 const arrow = document.querySelector('#arrow');
 const about = document.querySelector(`#about`);
 const gap = document.querySelector('#gap');
+const navbar = document.querySelector('.nav-container');
+const home = document.querySelector(`#home`);
 
-function handleClick() {
+function handleClick(e) {
+  console.log(e.target);
   console.log('🐛 IT GOT CLICKED!!!');
   // when clicked, go to the next section
   // 1. get the next section
@@ -26,3 +29,24 @@ function arrowMouseout() {
 workBtn.addEventListener('mouseover', arrowMouseover);
 workBtn.addEventListener(`mouseout`, arrowMouseout);
 workBtn.addEventListener(`click`, handleClick);
+
+// Navbar intersection observer
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = '0';
+  } else {
+    navbar.style.top = '-100px';
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+const navObs = new IntersectionObserver(
+  (entries) => {
+    navbar.classList.toggle(`active`, entries[0].isIntersecting);
+  },
+  { threshold: 0.75 }
+);
+
+navObs.observe(navbar);
