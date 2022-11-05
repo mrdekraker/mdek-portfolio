49d5384cc3901b25692df89c1eafc48dc501b8be
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-11-04 21:55:41 -0500

    rework portfolio start

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const workBtn = document.querySelector('.workBtn');
+const btn = document.querySelector('button');
+const arrow = document.querySelector('.arrow');
+const about = document.querySelector(`#about`);
+
+function handleClick() {
+  console.log('🐛 IT GOT CLICKED!!!');
+  // when clicked, go to the next section
+  // 1. get the next section
+  // 2. scroll to it
+  about.scrollIntoView({ behavior: 'smooth' });
+}
+
+function arrowMouseover() {
+  // console.log('🐛 IT GOT HOVERED!!!');
+  arrow.style.transform = 'scale(1.4)';
+}
+
+function arrowMouseout() {
+  // console.log('🐛 IT GOT UNHOVERED!!!');
+  arrow.style.transform = 'scale(1)';
+}
+
+// workBtn mouseover event
+workBtn.addEventListener('mouseover', arrowMouseover);
+workBtn.addEventListener(`mouseout`, arrowMouseout);
+workBtn.addEventListener(`click`, handleClick);
